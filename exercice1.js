11ff49fd78236416482655047681d05d4649f6fd
sstienface <s.stien@fondationface.org>
2018-10-26 14:01:54 +0200

    Initial Commit

---
+++ exercice1.js
@@ -0,0 +1,15 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+
+
+var prenoms = ["pierre","paul","jacques"];
+var x = ;
+
+document.getElementById('viewport'). = ;
+
+
+
+
+
---
+++ exercice2/exercice2.js
@@ -0,0 +1,15 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+
+
+var prenoms = ["pierre","paul","jacques"];
+    = "<Ton Prénom>";
+
+document.getElementById('viewport'). = ;
+
+
+
+
+
